87fb7c019ff89644cb75c5f02ee7167e5715271f
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-20 15:18:02 +0530

    Almost new proxy settings.

--- setup_redsocks.sh
+++ setup_redsocks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Originally written by przemoc.net. 
+# First modified and shared by Rajesh Veeankani. This is a clone.
+# Modified by Dilawar. 
 echo "Executing the script..."
 if [ ! -f ./redsocks.zip ];
 then 
@@ -16,9 +19,9 @@ clear
 echo -e "Compiling redsocks\n"
 make
 sudo cp ./redsocks /usr/local/sbin/redsocks
-sudo cp ./debian/init.d /etc/init.d/redsocks
-sudo cp ./debian/redsocks.default /etc/default/redsocks
-sudo chmod +x /etc/init.d/redsocks
+#sudo cp ./debian/init.d /etc/init.d/redsocks
+#sudo cp ./debian/redsocks.default /etc/default/redsocks
+#sudo chmod +x /etc/init.d/redsocks
 package=redsocks
 if [ $? -eq 0 ]
         then echo -e "$package install succeeded\n"
@@ -37,10 +40,8 @@ touch redsocks.conf
 echo "base {
  log_debug = on;
  log_info = on;
- log = \"stderr\";
- daemon = on;\
- user = redsocks;
- group = redsocks;
+ log = \"file:/tmp/redsock.log\";
+ daemon = on;
  redirector = iptables;
 }
 redsocks {
@@ -51,6 +52,7 @@ redsocks {
  local_ip = 127.0.0.1;
  local_port = 5123;
 // 'ip' and 'port' are IP and tcp-port of proxy-server
+// This is the ip of netmon.iitb.ac.in
  ip = 10.201.13.50;
  port = 80;
 // known types: socks4, socks5, http-connect, http-relay
@@ -66,11 +68,12 @@ ip = 10.201.13.50;
 type = http-connect;
 login = \"$username\";
  password = \"$pass\";
-}" > ~/.redsocks.conf
-sudo cp ~/.redsocks.conf /etc/redsocks.conf
+}" > $HOME/.redsocks.conf
 }
 
-if [ -f ~/.redsocks.conf ];
+redsocks_conf_set 
+
+if [ -f $HOME/.redsocks.conf ];
 then 
   echo "Configuration file .redsocks.conf is created in your home folder."
 else 
@@ -78,30 +81,3 @@ else
   exit
 fi 
 
-redirect_rules_set(){
-touch redirect.rules
-# This is from http://pritambaral.com/2012/04/transparent-proxy-on-linux/
-echo "*nat
-:PREROUTING ACCEPT [0:0]
-:INPUT ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-:POSTROUTING ACCEPT [0:0]
--A OUTPUT -d 10.0.0.0/8 -j RETURN
--A OUTPUT -d 127.0.0.0/8 -j RETURN
--A OUTPUT -d 192.168.0.0/16 -j RETURN
--A OUTPUT -o eth0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 127.0.0.1:5123
--A OUTPUT -o eth0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 127.0.0.1:5124
--A OUTPUT -o wlan0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 127.0.0.1:5123
--A OUTPUT -o wlan0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 127.0.0.1:5124
-COMMIT" > redirect.rules
-sudo iptables-restore < redirect.rules
-}
-redsocks_conf_set
-redirect_rules_set
-clear
-echo -e "\nPress yes whenever asked from here onwards\n"
-echo -e "\nStarting redsocks redirctor\n"
-sudo /usr/local/sbin/redsocks -c ~/.redsocks.conf
-echo -e "\nDO NOT SET-UP IT ON A SHARED MACHINE. IT SETS THE PROXY SYSTEM WIDE.\n"
-echo -e "\nFinished...\n "
-cd ..
